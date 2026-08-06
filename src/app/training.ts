b86e4f4fda8f491e51babc5c8f442001d23e5b4e
Ali <dr.magomedovali>
2026-08-06 12:20:09 +0300

    corrected errors

--- src/app/training.ts
+++ src/app/training.ts
@@ -1,56 +1,3 @@
-interface IUser {
-  id: number;
-  name: string;
-  email: string;
-  age?: number;
-}
-
-interface IEmployee extends IUser {
-  position: string;
-  salary: number;
-}
-
-const user: IEmployee = {
-  id: 1,
-  name: "Zakir",
-  email: "Zakir@TestBed.com",
-  position: "Разрабочик",
-  salary: 500000
-}
-
-
-function sum(a: number, b: number): number {
-  return a + b;
-}
-const result: number = sum(1, 2);
-console.log(result);
-
-
-let currentStatus: 'loading' | 'success' | 'error';
-
-currentStatus = 'loading';
-
-console.log(currentStatus);
-
-
-function formatText(str: string, format: 'uppercase' | 'lowercase'): string {
-  if (format === 'uppercase') {
-    return str.toUpperCase();
-  } else {
-    return str.toLowerCase();
-  }
-}
-formatText('Zakir', 'uppercase');
-
-
-function removeSpaces(text: string, symbol: string): string {
-  return text.replaceAll(symbol, '');
-}
-u
-const noSpaces: string = removeSpaces('К о д', ' ');
-console.log(noSpaces);
-
-
 const users: IUser[] = [
   { id: 2, name: 'Ольга', age: 25, email: 'Olga@test.com' },
   { id: 3, name: 'Али', age: 12, email: 'Ali@test.com' },
@@ -103,5 +50,3 @@ export function suma(a: number, b: number): number {
 const filteredByName = users.filter((user: IUser): boolean => {
   return user.name.startsWith('A')
 })
-
-
